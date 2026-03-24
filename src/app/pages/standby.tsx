1916d632af25952f018c2df7b3d2ffff5da6ca20
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2026-03-24 03:11:04 -0300

    Encapsulated date formatting and unit tests

--- src/app/pages/standby.tsx
+++ src/app/pages/standby.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import HomeScreen from '@/shared/ui/home-screen';
 import { useTimerController } from '@/features/clock/infrastructure/controllers/use-timer-controller';
 import { useSettingsStore } from '@/features/settings/state/settings-store';
-import { formatClockTime } from '@/shared/utils/date-time.util';
+import { formatClockTime, formatDateTime } from '@/shared/utils/date-time.util';
 
 const StandbyBody = styled.div`
   display: flex;
@@ -39,8 +39,8 @@ const StandbyPage = () => {
   return (
     <HomeScreen>
       <StandbyBody>
-        <TimeLabel>{formatClockTime(date, locale).slice(0, 5)}</TimeLabel>
-        <DateLabel>{date.toLocaleDateString(locale)}</DateLabel>
+        <TimeLabel>{formatClockTime(date, { locale })}</TimeLabel>
+        <DateLabel>{formatDateTime(date, { locale })}</DateLabel>
         <HintLabel>Press Menu</HintLabel>
       </StandbyBody>
     </HomeScreen>
--- src/shared/utils/date-time.util.ts
+++ src/shared/utils/date-time.util.ts
@@ -1,3 +1,47 @@
-export const formatClockTime = (date: Date, locale: string = 'pt-br') => {
-  return date.toLocaleTimeString(locale);
+interface IFormatClockTimeProps {
+  locale?: Intl.LocalesArgument;
+  options?: Intl.DateTimeFormatOptions;
+}
+
+const DEFAULT_FORMAT_CLOCK_TIME_PROPS: Required<IFormatClockTimeProps> = {
+  locale: 'en-US',
+  options: {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  },
+};
+
+const DEFAULT_FORMAT_DATE_TIME_PROPS: Required<IFormatClockTimeProps> = {
+  locale: 'en-US',
+  options: {
+    day: '2-digit',
+    month: '2-digit',
+  },
+};
+
+export const formatClockTime = (
+  date: Date,
+  {
+    locale = DEFAULT_FORMAT_CLOCK_TIME_PROPS.locale,
+    options = DEFAULT_FORMAT_CLOCK_TIME_PROPS.options,
+  }: IFormatClockTimeProps = DEFAULT_FORMAT_CLOCK_TIME_PROPS
+) => {
+  return date.toLocaleTimeString(locale, {
+    ...DEFAULT_FORMAT_CLOCK_TIME_PROPS.options,
+    ...options,
+  });
+};
+
+export const formatDateTime = (
+  date: Date,
+  {
+    locale = DEFAULT_FORMAT_DATE_TIME_PROPS.locale,
+    options = DEFAULT_FORMAT_DATE_TIME_PROPS.options,
+  }: IFormatClockTimeProps = DEFAULT_FORMAT_DATE_TIME_PROPS
+) => {
+  return date.toLocaleDateString(locale, {
+    ...DEFAULT_FORMAT_DATE_TIME_PROPS,
+    ...options,
+  });
 };
